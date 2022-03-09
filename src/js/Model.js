d51d6a93cd65c357a3fcab940a511eb7770328ab
BB-choi <choibobae0809@gmail.com>
2022-03-09 15:28:51 +0900

    Feat: create town data to Model

--- src/js/Model.js
+++ src/js/Model.js
@@ -1,3 +1,31 @@
-class Model {}
+import { getRandomNumber } from './util/utils.js';
+import {
+  MAX_TOWN_COUNT,
+  MIN_TOWN_COUNT,
+  WRAP_COUNT,
+} from './util/constants.js';
+
+class Model {
+  constructor() {
+    this.wrapCount = WRAP_COUNT;
+    this.maxTownCount = MAX_TOWN_COUNT;
+    this.minTownCount = MIN_TOWN_COUNT;
+    this.townNameIdx = 0;
+    this.townData = {};
+  }
+
+  get nestedTownCount() {
+    return getRandomNumber(this.minTownCount, this.maxTownCount);
+  }
+
+  createData() {
+    this.townData = {
+      location: { top: 10, left: 10 },
+      size: { width: 30, heigth: 90 },
+      nameIdx: 0,
+      children: [],
+    };
+  }
+}
 
 export { Model };
---
+++ src/js/util/constants.js
@@ -0,0 +1,5 @@
+const MAX_TOWN_COUNT = 7;
+const MIN_TOWN_COUNT = 1;
+const WRAP_COUNT = 4;
+
+export { MAX_TOWN_COUNT, MIN_TOWN_COUNT, WRAP_COUNT };
